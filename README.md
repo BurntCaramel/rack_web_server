ec9ae85b39e8ba1d6e737ca79f8f6220cdf74118
Patrick Smith <patrick@burntcaramel.com>
2017-03-24 18:24:15 +1100

    Add static file server

--- README.md
+++ README.md
@@ -11,3 +11,17 @@ gem install bundler
 source 'https://rubygems.org'
 gem 'rack', '~> 2.0.1'
 ```
+
+```
+# Only has one response for all requests
+ruby server_01_simple.rb
+
+# Has different page content for certain paths
+ruby server_02_pages.rb
+
+# Sends the correct status code for not found pages
+ruby server_03_status.rb
+
+# Handles trailing slash redirects
+ruby server_04_redirect.rb
+```
---
+++ server_04_redirect.rb
@@ -0,0 +1,62 @@
+require 'rack'
+require 'rack/server'
+
+class App
+  def initialize
+    @paths_to_content = {
+      '/' => {
+        statusCode: 200,
+        html: 'Welcome to the home page'
+      },
+      '/about' => {
+        statusCode: 200,
+        html: 'About me'
+      },
+      '/contact' => {
+        statusCode: 200,
+        html: 'You can contact me by the methods below'
+      }
+    }
+  end
+
+  def content_for_path(path)
+    @paths_to_content.fetch(path) do |path|
+      # If we have a trailing slash
+      if path[-1] === '/'
+        path_without_trailing_slash = path[0..-2]
+        if @paths_to_content.include?(path_without_trailing_slash)
+          next redirect_content_to(path_without_trailing_slash)
+        end
+      end
+      not_found_content
+    end
+  end
+
+  def redirect_content_to(path)
+    return {
+      statusCode: 302,
+      headers: {
+        'Location' => path
+      }
+    }
+  end
+
+  def not_found_content
+    return {
+      statusCode: 404,
+      html: 'Not Found'
+    }
+  end
+
+  def call(env)
+    method = env['REQUEST_METHOD']
+    path = env['REQUEST_PATH']
+    content = content_for_path(path)
+    statusCode = content[:statusCode]
+    headers = content.fetch(:headers, {})
+    html = content.fetch(:html, '')
+    [statusCode, headers, [html]]
+  end
+end
+
+Rack::Server.start app: App.new
